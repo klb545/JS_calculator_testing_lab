e75eb41d614cb2be570117ef29b9c252af189750
klb545 <126502552+klb545@users.noreply.github.com>
2023-05-03 20:04:07 +0100

    made even and odd functions more concise

--- calculator.js
+++ calculator.js
@@ -8,23 +8,11 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
-const modulus = (a, b) => a % b === -0 ? 0 : (a * b < 0 ? a % b + b : a % b);
+const modulus = (a, b) => a%b === -0 ? 0 : (a*b < 0 ? a%b+b : a%b);
 
-const even = function(a){
-    if(a % 2 === 0){
-        return true;
-    } else {
-        return false;
-    }
-};
+const even = (a) => a % 2 === 0 ? true : false;
 
-const odd = function(a){
-    if(a % 2 === 0){
-        return false;
-    } else {
-        return true;
-    }
-};
+const odd = (a) => a % 2 === 0 ? false : true;
 
 module.exports = { 
     sum, 
