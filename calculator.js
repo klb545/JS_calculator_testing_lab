1f22933ee6f8ad3843ed7c8e8c9ed1d235613bb8
klb545 <126502552+klb545@users.noreply.github.com>
2023-05-03 19:57:13 +0100

    made modulus function more concise

--- calculator.js
+++ calculator.js
@@ -12,7 +12,7 @@ const modulus = (a, b) => {
     if (a % b === -0) {
         return 0;
     }
-    return a * b < 0 ? b + (a % b) : a % b;
+    return a * b < 0 ? a % b + b : a % b;
 };
 
 const even = function(a){
